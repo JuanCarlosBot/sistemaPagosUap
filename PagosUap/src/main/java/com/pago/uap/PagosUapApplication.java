4de9b00787eae72e85bcdcd5bce6074f39e02166
JuanCarlosBot <52556101+JuanCarlosBot@users.noreply.github.com>
2023-05-22 16:18:14 -0400

    creacion de proyecto
    v1

---
+++ PagosUap/src/main/java/com/pago/uap/PagosUapApplication.java
@@ -0,0 +1,13 @@
+package com.pago.uap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosUapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosUapApplication.class, args);
+	}
+
+}
---
+++ PagosUap/src/test/java/com/pago/uap/PagosUapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pago.uap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosUapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
